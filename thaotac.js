773f83f9f5fdfbb7abb95c18278b458591bca2f5
Chủ Nhân <ltdatktpm2311004@student.ctuet.edu.vn>
2025-07-10 22:57:35 +0700

    update__lan1

---
+++ thaotac.js
@@ -0,0 +1,32 @@
+const temp = document.getElementById("update");
+const temp2 = document.getElementById("khoiupdate");
+temp.addEventListener("click", function (e) {
+  e.preventDefault();
+  temp2.classList.remove("khoitanghinh");
+});
+const temp3 = document.getElementById("cancel");
+temp3.addEventListener("click", function (e) {
+  e.preventDefault();
+  temp2.classList.add("khoitanghinh");
+});
+const temp4 = document.getElementById("save");
+temp4.addEventListener("click", function (e) {
+  e.preventDefault();
+  temp2.classList.add("khoitanghinh");
+});
+const temp_delete = document.getElementById("delete");
+const temp__war = document.getElementById("khoidelete");
+const temp5 = document.getElementById("No");
+const temp6 = document.getElementById("Yes");
+temp_delete.addEventListener("click", function (e) {
+  e.preventDefault();
+  temp__war.classList.remove("khoitanghinh");
+});
+temp5.addEventListener("click", function (e) {
+  e.preventDefault();
+  temp__war.classList.add("khoitanghinh");
+});
+temp6.addEventListener("click", function (e) {
+  e.preventDefault();
+  temp__war.classList.add("khoitanghinh");
+});
